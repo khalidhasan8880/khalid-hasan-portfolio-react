b8762d0d26efe9deb88f39abcba7c37340d0bb81
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-07-15 10:03:22 +0600

    project done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,7 +21,6 @@ const Navbar = () => {
   useEffect(() => {
     const handleScroll = () => {
       // const home = document.getElementById('')
-      const home = document.getElementById("home");
       const about = document.getElementById("about");
       const projects = document.getElementById("projects");
       const contact = document.getElementById("contact");
